7f97e6e3b58fad3ce421ee82f9ad03f8ce126875
Prakash <srisurya092@gmail.com>
2018-04-01 22:41:22 +0530

    Add production configuration.

---
+++ webpack.production.config.js
@@ -0,0 +1,43 @@
+var copyWebpackPlugin = require("copy-webpack-plugin");
+var path = require('path');
+
+module.exports = {
+	entry: {
+		bundle: './App/index.tsx'
+	},
+	
+	output: {
+		filename: 'bundle/[name].js',
+		path: path.resolve(__dirname, "")
+	},
+	
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader'
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			'App': path.resolve(__dirname, "App/"),
+			'Server': path.resolve(__dirname, "Server/"),
+			'Schema': path.resolve(__dirname, "Schema/")
+		},
+		extensions: [".tsx", ".ts", ".js"]
+	},
+	plugins: [
+		new copyWebpackPlugin([{
+			from: './node_modules/classui/assets/font-awesome',
+			to: './bundle/font-awesome'
+		}])
+	],
+	mode: "production",
+	
+	devServer: {
+		host: '0.0.0.0',
+		historyApiFallback: true
+	}
+};
