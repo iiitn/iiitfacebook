09ae7aae7ce031c9e2fb0008509ed270bed2c92d
Kishore <kishore.iiitn@gmail.com>
2018-04-01 21:01:33 +0530

    Update schema to include 2014 batch and extend IUserSchema interface.

--- Schema/User.ts
+++ Schema/User.ts
@@ -5,7 +5,8 @@ export let UserSchema: IJSONSchema = {
 	additionalProperties: false,
 	properties: {
 		_id: {
-			type: "string"
+			type: "string",
+			pattern: "^N[0-9]{6}$"
 		},
 		name: {
 			type: "string"
@@ -15,7 +16,7 @@ export let UserSchema: IJSONSchema = {
 			minLength: 5
 		},
 		batch: {
-			enum: ["2008", "2009", "2010", "2011", "2012", "2013", "2015"]
+			enum: ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]
 		},
 		branch: {
 			enum: ["CSE", "ECE", "MECHANICAL", "CHEMICAL", "MME", "CIVIL"]
@@ -30,9 +31,12 @@ export let UserSchema: IJSONSchema = {
 	required: ["_id", "name", "password", "gender", "batch", "branch", "class"]
 }
 
-interface IUserSchema {
+export interface IUserSchema {
 	_id: string
 	name: string
 	password: string
 	gender: "male"|"female"
+	batch: "2008" | "2009" | "2010" | "2011" | "2012" | "2013" | "2014" | "2015"
+	class: 1|2|3|4|5|6
+	branch: "CSE" | "ECE" | "MECHANICAL" | "CHEMICAL" | "MME" | "CIVIL"
 }
